3bfe1a42a8e5aa367d352194ad66b1341e43b4fe
nipfty <kingstimeent@gmail.com>
2018-04-22 18:46:26 +0200

    Attempts at Jump scripting

--- SGMCourseAssignment/Assets/Scripts/JumpingScript.cs
+++ SGMCourseAssignment/Assets/Scripts/JumpingScript.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class JumpingScript : MonoBehaviour {
 
+    [SerializeField] float force;
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(Input.GetKey(KeyCode.W))
+        rb.AddForce(transform.up*force);
 	}
 }
