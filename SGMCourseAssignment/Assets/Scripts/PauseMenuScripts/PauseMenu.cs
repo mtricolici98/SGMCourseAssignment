76233e4ccf7e8e60e7ab0afacf6e30ab2ca0f8dc
Zahari Dzhelepov <253925@via.dk>
2018-04-23 11:46:47 +0200

    PauseMenu
    pause menu almost finished

---
+++ SGMCourseAssignment/Assets/Scripts/PauseMenuScripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool gamePaused = false;
+	public GameObject pauseMenuUI;
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if(gamePaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+	}
+	public void Resume(){
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		gamePaused = false;
+	}
+	void Pause(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		gamePaused = true;
+	}
+
+	
+}
---
+++ SGMCourseAssignment/Assets/Scripts/PauseMenuScripts/QuitButtonPause.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButtonPause : MonoBehaviour {
+
+	public void QuitGame(){
+		Application.Quit();
+		Debug.Log("Quitting...");
+	}
+}
