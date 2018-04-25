3f6267cbf5bf644171af363433a5c7cc4b90b28e
nipfty <kingstimeent@gmail.com>
2018-04-25 16:55:21 +0200

    RandomPowerUpSpawner

--- SGMCourseAssignment/Assets/Scripts/RandomPowerUpSpawner.cs
+++ SGMCourseAssignment/Assets/Scripts/RandomPowerUpSpawner.cs
@@ -7,13 +7,41 @@ public class RandomPowerUpSpawner : MonoBehaviour {
 
     [SerializeField]  GameObject powerBubble;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float timeUntilSpawn;
+    // Use this for initialization
+    void Start () {
+        timeUntilSpawn = Random.Range(10, 20);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        timeUntilSpawn -= Time.deltaTime;
+        if (timeUntilSpawn <= 0)
+        {
+            SpawnPowerup();
+         timeUntilSpawn = Random.Range(10, 20); ;
+        }
+        
+        cleanUp();
+    }
+
+    private void SpawnPowerup()
+    {
+        Vector3 newPos = new Vector3(Random.Range(-15, 15), Random.Range(9  , 10), 0);
+        GameObject octo = Instantiate(powerBubble, newPos, Quaternion.identity) as GameObject;
+
+    }
+
+    private void cleanUp()
+    {
+        GameObject[] powerups = GameObject.FindGameObjectsWithTag("PowerUp");
+        foreach(GameObject powerup in powerups){
+            if (powerup.transform.position.y < -9f)
+            {
+                Destroy(powerup);
+            }
+        }
+    }
+
+    
 }
