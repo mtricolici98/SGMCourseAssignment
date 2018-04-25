1999c1134fb5aef0df127f2679bfd7367745f621
nipfty <kingstimeent@gmail.com>
2018-04-25 16:23:49 +0200

    WorkingOnPowerups

---
+++ SGMCourseAssignment/Assets/Scripts/RandomPowerUpSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPowerUpSpawner : MonoBehaviour {
+
+
+    [SerializeField]  GameObject powerBubble;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
