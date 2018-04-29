564160c42532c0f470667eb3a568e4711d887998
nipfty <kingstimeent@gmail.com>
2018-04-29 14:56:04 +0200

    powerUpUpdate

--- SGMCourseAssignment/Assets/Scripts/PowerEffectScript.cs
+++ SGMCourseAssignment/Assets/Scripts/PowerEffectScript.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerEffectScript : MonoBehaviour {
-
+    private bool isBig,isFrozen,isBiggerGate,isSmallerGate;
 	// Use this for initialization
 	void Start () {
-		
+        isBig = isFrozen = isBiggerGate = isSmallerGate = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isBig)
+        {
+            Invoke("makeBig", 0f);
+        }
 	}
 
 
     void OnTriggerEnter2D(Collider2D col)
     {
+      
         Debug.Log("COLIDED:  "  +col.gameObject.name);
         if(col.gameObject.name == "Grow(Clone)")
         {
             Debug.Log("GROWING");
+            isBig = true;
+          
         }
         Destroy(col.gameObject);
+    }
+
+    void makeBig()
+    {
+        gameObject.transform.localScale = new Vector2(1f, 1f);
     }
 
 
