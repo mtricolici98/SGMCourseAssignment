d09a9c57e1e234093fe98576909b9cdbad147d41
Zahari Dzhelepov <253925@via.dk>
2018-05-01 22:52:43 +0200

    kick animation without actual kicking of the ball

---
+++ SGMCourseAssignment/Assets/KickingScript1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickingScript1 : MonoBehaviour {
+
+	Animator animator ;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightShift)){
+			animator.Play("kicking_anim",-1,0f);
+		}
+	}
+}
---
+++ SGMCourseAssignment/Assets/Scripts/KickingScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickingScript : MonoBehaviour {
+
+Animator animator ;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftShift)){
+			animator.Play("kicking",-1,0f);
+		}
+	}
+}
