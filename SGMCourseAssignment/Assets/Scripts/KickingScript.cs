620097b7663dbcfcb0673bea1116f9e989012fff
nipfty <kingstimeent@gmail.com>
2018-05-02 16:08:37 +0200

    KickingScriptWorking

--- SGMCourseAssignment/Assets/Scripts/KickingScript.cs
+++ SGMCourseAssignment/Assets/Scripts/KickingScript.cs
@@ -19,19 +19,25 @@ public GameObject ball;
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)){
-			hit = Physics2D.Raycast(transform.position + new Vector3(1f,1f,0f),ball.transform.position);
-			Debug.DrawLine(transform.position,ball.transform.position, Color.red , 10f);
-			if(hit.collider.gameObject.tag == "ball"){
-			 Rigidbody2D ballrb = ball.GetComponent<Rigidbody2D>();	
-			 ballrb.AddForce(new Vector3(50f,50f,0f));
-			}
+        if (Input.GetKeyDown(KeyCode.Space)) {
+         
 	}
 
 		if(Input.GetKeyDown(KeyCode.LeftShift)){
 			animator.Play("kicking",-1,0f);
-			
-		}
+            // Debug.Log("Space Hit");
+            hit = Physics2D.Raycast(transform.position + transform.right, transform.right);
+            // Debug.Log("Transform right " + transform.right);
+            //  Debug.DrawLine(transform.position + transform.right, transform.right, Color.red, 10f);
+            Debug.Log("Raycasted with  " + hit.collider.gameObject.tag);
+            if (hit.collider.gameObject.tag == "ball" && hit.distance < 1f)
+            {
+                //Debug.Log("Distance : " + hit.distance);
+                //Debug.Log("RayCasted with  " + hit);
+                Rigidbody2D ballrb = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                ballrb.AddForce(transform.up * force, ForceMode2D.Impulse);
+            }
+        }
 		}
 	}
 
