52319b3db9330380484035194a4d8caa8ada4bec
nipfty <kingstimeent@gmail.com>
2018-05-02 14:37:35 +0200

    GameManagerStart

---
+++ SGMCourseAssignment/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    //  private BoardManager boardScript;                     
+
+    private int player1Score;
+    private int player2Score;
+    void Awake()
+    {
+
+        if (instance == null)
+
+
+            instance = this;
+
+
+        else if (instance != this)
+
+
+            Destroy(gameObject);
+
+
+        DontDestroyOnLoad(gameObject);
+
+
+        InitGame();
+    }
+
+
+    void InitGame()
+    {
+
+        //  boardScript.SetupScene(level);
+        player1Score = 0;
+        player2Score = 0;
+    }
+
+
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        ScoreManager.OnScoreL += ScoreL;
+        ScoreManager.OnScoreR += ScoreR;
+    } 
+
+    void OnDisable()
+    {
+        ScoreManager.OnScoreL -= ScoreL;
+        ScoreManager.OnScoreR -= ScoreR;
+    }
+
+    void ScoreL()
+    {
+
+    }
+
+    void ScoreR()
+    {
+
+    }
+
+}
---
+++ SGMCourseAssignment/Assets/Scripts/GameManagement/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+
+    public delegate void ScoredL();
+    public static event ScoredL OnScoreL;
+    public delegate void ScoredR();
+    public static event ScoredR OnScoreR;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "ball")
+        {
+            if (gameObject.tag == "GateP1")
+            {
+                OnScoreL();
+            } else if (gameObject.tag == "GateP2")
+            {
+                OnScoreR();
+            }
+        } 
+    }
+}
